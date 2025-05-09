44434b920ec830d4bda9d7f0f522b833ab538b9f
FindMoreLuck <liam.carlier15@gmail.com>
2025-05-09 21:33:23 +0200

    script for item stats

---
+++ Assets/Scripts/WIP[Liam]/Room/ItemStats.cs
@@ -0,0 +1,14 @@
+using UnityEngine;
+
+public class ItemStats : MonoBehaviour
+{
+    private int _hp;
+    public bool IsGlass, IsWood;
+    private float _placementTime;
+   
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
