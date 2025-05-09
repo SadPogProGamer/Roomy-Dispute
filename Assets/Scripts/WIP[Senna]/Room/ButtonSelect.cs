f66b98db6b3a0a72f25ff115b56bb41bd728c9b9
SadPogProGamer <cloetsenna@gmail.com>
2025-05-09 18:47:29 +0200

    buttons

--- Assets/Scripts/WIP[Senna]/Room/ButtonSelect.cs
+++ Assets/Scripts/WIP[Senna]/Room/ButtonSelect.cs
@@ -7,29 +7,38 @@ public class ButtonSelect : MonoBehaviour
     [SerializeField] private Image _cashAppImage;
     [SerializeField] private Image _sabotageAppImage;
 
+    //[Header("Furniture Images")]
+    //[SerializeField] private Image _furnitureImage;
+
+    [Header("Sabotage Images")]
+    [SerializeField] private Image _fireImage;
+    [SerializeField] private Image _bombImage;
+
     private InputAction _cancelAction;
     private InputAction _southAction;
-    private InputAction _eastAction; 
+    private InputAction _eastAction;
 
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
+        _fireImage.enabled = false;
+        _bombImage.enabled = false;
+
         _cancelAction = new InputAction("Cancel", binding: "<Gamepad>/buttonEast");
         _cancelAction.performed += OnCancel;
-        _cancelAction.Enable(); 
+        _cancelAction.Enable();
 
         _southAction = new InputAction("South", binding: "<Gamepad>/buttonSouth");
-        _southAction.performed += OnSelect; 
+        _southAction.performed += OnSelect;
         _southAction.Enable();
     }
 
     // Update is called once per frame
     void Update()
     {
-        // You can add other update logic here if needed
+
     }
 
-    // Called when the South button (A or Circle) is pressed
     private void OnSelect(InputAction.CallbackContext context)
     {
         // Toggle images based on which app is selected
@@ -64,12 +73,13 @@ public class ButtonSelect : MonoBehaviour
 
     private void ShowSabotageImages()
     {
-        
+        _fireImage.enabled = true;
+        _bombImage.enabled = true;
     }
 
     void OnCancel(InputAction.CallbackContext context)
     {
-        _sabotageAppImage.enabled = true;
-        _cashAppImage.enabled = true;
+        _sabotageAppImage.enabled = false;
+        _cashAppImage.enabled = false;
     }
 }
