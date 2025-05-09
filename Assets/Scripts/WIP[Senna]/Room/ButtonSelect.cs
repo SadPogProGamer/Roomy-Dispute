4aedbfce6543a402eb1a18f48363fe21a60770e2
SadPogProGamer <cloetsenna@gmail.com>
2025-05-09 18:34:20 +0200

    new

--- Assets/Scripts/WIP[Senna]/Room/ButtonSelect.cs
+++ Assets/Scripts/WIP[Senna]/Room/ButtonSelect.cs
@@ -8,21 +8,19 @@ public class ButtonSelect : MonoBehaviour
     [SerializeField] private Image _sabotageAppImage;
 
     private InputAction _cancelAction;
-    private InputAction _southAction; // Input action for "A" button on Xbox / "Circle" on PS
-    private InputAction _eastAction; // Input action for "B" button on Xbox / "Cross" on PS (Cancel)
+    private InputAction _southAction;
+    private InputAction _eastAction; 
 
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
-        // Input action for cancel (Escape button)
-        _cancelAction = new InputAction("Cancel", binding: "<Gamepad>/buttonEast"); // This listens for "B" (Xbox) or "Cross" (PS)
-        _cancelAction.performed += OnCancel; // Subscribe to the cancel event
-        _cancelAction.Enable(); // Enable the action
+        _cancelAction = new InputAction("Cancel", binding: "<Gamepad>/buttonEast");
+        _cancelAction.performed += OnCancel;
+        _cancelAction.Enable(); 
 
-        // Input action for "A" button on Xbox / "Circle" button on PS (South button)
         _southAction = new InputAction("South", binding: "<Gamepad>/buttonSouth");
-        _southAction.performed += OnSouthPressed; // Subscribe to the performed event
-        _southAction.Enable(); // Enable the action
+        _southAction.performed += OnSelect; 
+        _southAction.Enable();
     }
 
     // Update is called once per frame
@@ -32,7 +30,7 @@ public class ButtonSelect : MonoBehaviour
     }
 
     // Called when the South button (A or Circle) is pressed
-    private void OnSouthPressed(InputAction.CallbackContext context)
+    private void OnSelect(InputAction.CallbackContext context)
     {
         // Toggle images based on which app is selected
         if (_cashAppImage.enabled)
@@ -66,13 +64,11 @@ public class ButtonSelect : MonoBehaviour
 
     private void ShowSabotageImages()
     {
-        // Implement showing sabotage images logic here
+        
     }
 
-    // Called when the cancel (B or Cross) button is pressed
     void OnCancel(InputAction.CallbackContext context)
     {
-        // Re-enable both images
         _sabotageAppImage.enabled = true;
         _cashAppImage.enabled = true;
     }
