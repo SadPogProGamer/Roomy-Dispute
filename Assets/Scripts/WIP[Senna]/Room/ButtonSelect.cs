e227d2daccae0dd8c4a932698081002a93679d37
SadPogProGamer <cloetsenna@gmail.com>
2025-05-09 18:28:05 +0200

    buttons

--- Assets/Scripts/WIP[Senna]/Room/ButtonSelect.cs
+++ Assets/Scripts/WIP[Senna]/Room/ButtonSelect.cs
@@ -1,25 +1,48 @@
-using System;
-using TMPro;
 using UnityEngine;
 using UnityEngine.InputSystem;
 using UnityEngine.UI;
 
 public class ButtonSelect : MonoBehaviour
 {
-
     [SerializeField] private Image _cashAppImage;
     [SerializeField] private Image _sabotageAppImage;
+
+    private InputAction _cancelAction;
+    private InputAction _southAction; // Input action for "A" button on Xbox / "Circle" on PS
+    private InputAction _eastAction; // Input action for "B" button on Xbox / "Cross" on PS (Cancel)
 
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
+        // Input action for cancel (Escape button)
+        _cancelAction = new InputAction("Cancel", binding: "<Gamepad>/buttonEast"); // This listens for "B" (Xbox) or "Cross" (PS)
+        _cancelAction.performed += OnCancel; // Subscribe to the cancel event
+        _cancelAction.Enable(); // Enable the action
 
+        // Input action for "A" button on Xbox / "Circle" button on PS (South button)
+        _southAction = new InputAction("South", binding: "<Gamepad>/buttonSouth");
+        _southAction.performed += OnSouthPressed; // Subscribe to the performed event
+        _southAction.Enable(); // Enable the action
     }
 
     // Update is called once per frame
     void Update()
     {
+        // You can add other update logic here if needed
+    }
 
+    // Called when the South button (A or Circle) is pressed
+    private void OnSouthPressed(InputAction.CallbackContext context)
+    {
+        // Toggle images based on which app is selected
+        if (_cashAppImage.enabled)
+        {
+            IsCashAppPressed();
+        }
+        else if (_sabotageAppImage.enabled)
+        {
+            IsSabotageAppPressed();
+        }
     }
 
     public void IsCashAppPressed()
@@ -31,7 +54,7 @@ public class ButtonSelect : MonoBehaviour
 
     private void ShowFurnitureImages()
     {
-        
+        // Implement showing furniture images logic here
     }
 
     public void IsSabotageAppPressed()
@@ -43,11 +66,13 @@ public class ButtonSelect : MonoBehaviour
 
     private void ShowSabotageImages()
     {
-       
+        // Implement showing sabotage images logic here
     }
 
-    void OnCancel(InputAction action)
+    // Called when the cancel (B or Cross) button is pressed
+    void OnCancel(InputAction.CallbackContext context)
     {
+        // Re-enable both images
         _sabotageAppImage.enabled = true;
         _cashAppImage.enabled = true;
     }
