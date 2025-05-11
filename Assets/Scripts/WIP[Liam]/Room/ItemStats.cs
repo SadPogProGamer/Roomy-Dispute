168f682ba2e9a75d683b5157244007685628f4c0
FindMoreLuck <liam.carlier15@gmail.com>
2025-05-11 15:37:52 +0200

    Update ItemStats.cs

--- Assets/Scripts/WIP[Liam]/Room/ItemStats.cs
+++ Assets/Scripts/WIP[Liam]/Room/ItemStats.cs
@@ -4,11 +4,17 @@ public class ItemStats : MonoBehaviour
 {
     public int HP, Cost, Points;
     public bool IsGlass, IsWood, IsPlaced;
-    public float PlacementTime;
+    public float PlacementTime, Timer;
+    
    
     // Update is called once per frame
     void Update()
     {
         if (HP <= 0) Destroy(gameObject);
+
+        if (IsPlaced && Timer >= PlacementTime)
+        {
+            Timer += Time.deltaTime;
+        }
     }
 }
