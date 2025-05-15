d3f313d310610c2c239b25c9f32b2f5c0b22766d
SadPogProGamer <cloetsenna@gmail.com>
2025-05-15 14:18:05 +0200

    push

--- Assets/Scripts/WIP[Liam]/Room/PlayersInstantiate.cs
+++ Assets/Scripts/WIP[Liam]/Room/PlayersInstantiate.cs
@@ -7,21 +7,30 @@ public class PlayersInstantiate : MonoBehaviour
     private GameObject _prefab, _moneyManager;
     [SerializeField]
     private int _playerCount;
+
+    [SerializeField] private Material[] _playerMaterials;
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
-        for (int plyrcnt = 0; plyrcnt< _playerCount; plyrcnt++)
+        for (int plyrcnt = 0; plyrcnt < _playerCount; plyrcnt++)
         {
             GameObject player = Instantiate(_prefab);
             player.transform.GetChild(0).GetComponent<ButtonSelect>().MoneyManager = _moneyManager;
             if (plyrcnt % 2 != 0)
+            {
                 player.transform.GetChild(0).GetChild(0).GetComponent<RectTransform>().anchoredPosition = new Vector2(-player.transform.GetChild(0).GetChild(0).GetComponent<RectTransform>().anchoredPosition.x, player.transform.GetChild(0).GetChild(0).GetComponent<RectTransform>().anchoredPosition.y);
-            if (plyrcnt > 1) player.transform.GetChild(0).GetChild(0).GetComponent<RectTransform>().anchoredPosition = new Vector2(player.transform.GetChild(0).GetChild(0).GetComponent<RectTransform>().anchoredPosition.x, -player.transform.GetChild(0).GetChild(0).GetComponent<RectTransform>().anchoredPosition.y);
-
-            
-
+            }
+            if (plyrcnt > 1)
+            {
+                player.transform.GetChild(0).GetChild(0).GetComponent<RectTransform>().anchoredPosition = new Vector2(player.transform.GetChild(0).GetChild(0).GetComponent<RectTransform>().anchoredPosition.x, -player.transform.GetChild(0).GetChild(0).GetComponent<RectTransform>().anchoredPosition.y);
+            }
             player.transform.GetChild(1).GetComponent<PlayerPointer>().PlayerIndex = plyrcnt;
 
+            Renderer rend = player.GetComponentInChildren<Renderer>();
+            if (rend != null && plyrcnt < _playerMaterials.Length)
+            {
+                rend.material = _playerMaterials[plyrcnt];
+            }
         }
     }
 }
