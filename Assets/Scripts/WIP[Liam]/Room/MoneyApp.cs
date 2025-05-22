e88798974f51ff31ddfa255823e658f0c1541052
FindMoreLuck <liam.carlier15@gmail.com>
2025-05-22 15:06:39 +0200

    trying the app thingy

---
+++ Assets/Scripts/WIP[Liam]/Room/MoneyApp.cs
@@ -0,0 +1,63 @@
+using UnityEngine;
+
+public class MoneyApp : MonoBehaviour
+{
+    private int _buttonIndex, _previousButtonIndex;
+    // Update is called once per frame
+
+    private void Start()
+    {
+        _buttonIndex = Random.Range(0, 8);
+    }
+    void Update()
+    {
+        if (_buttonIndex != _previousButtonIndex)
+        {
+            for (int childIndex = 0; childIndex < transform.childCount; childIndex++)
+            {
+                if(childIndex == _buttonIndex)
+                {
+                    transform.GetChild(childIndex).gameObject.SetActive(true);
+                }
+                else { transform.GetChild(childIndex).gameObject.SetActive(false); }
+            }
+        }
+    }
+
+
+    public void OnUp()
+    {
+        if (_buttonIndex == 0)
+        {
+
+        }
+    }
+    public void OnDown()
+    {
+
+    }
+    public void OnLeft()
+    {
+
+    }
+    public void OnRight()
+    {
+
+    }
+    public void OnA()
+    {
+
+    }
+    public void OnX()
+    {
+
+    }
+    public void OnY()
+    {
+
+    }
+    public void OnB()
+    {
+
+    }
+}
