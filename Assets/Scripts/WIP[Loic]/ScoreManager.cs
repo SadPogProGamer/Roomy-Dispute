a436217a43c685e6828da06d40551ad69e5f388b
lg6200 <gouwyloic@gmail.com>
2025-05-22 14:50:11 +0200

    Score3

--- Assets/Scripts/WIP[Loic]/ScoreManager.cs
+++ Assets/Scripts/WIP[Loic]/ScoreManager.cs
@@ -1,64 +1,64 @@
-using UnityEngine;
-using TMPro;
-using System.Collections.Generic;
+//using UnityEngine;
+//using TMPro;
+//using System.Collections.Generic;
 
-public class ScoreManager : MonoBehaviour
-{
-    [SerializeField] private TextMeshProUGUI[] _playerScoreTexts;
+//public class ScoreManager : MonoBehaviour
+//{
+//    [SerializeField] private TextMeshProUGUI[] _playerScoreTexts;
 
-    public List<int> PlayerScore = new List<int>();
-
-
-    private void Start()
-    {
-        for (int i = 0; i < _playerScoreTexts.Length; i++)
-        {
-            PlayerScore.Add(0);
-        }
-
-        UpdateAllScoreUI();
-    }
+//    public List<int> PlayerScore = new List<int>();
 
 
-    private void Update()
-    {
-        for (int i = 0; i < PlayerScore.Count; i++)
-        {
-            if (Input.GetKeyDown(KeyCode.Alpha1 + i))
-            {
-                IncreaseScore(i, 10);
-            }
-        }
-    }
+//    private void Start()
+//    {
+//        for (int i = 0; i < _playerScoreTexts.Length; i++)
+//        {
+//            PlayerScore.Add(0);
+//        }
 
-    public void IncreaseScore(int playerID, int amount)
-    {
-        if (playerID >= 0 && playerID < PlayerScore.Count)
-        {
-            PlayerScore[playerID] += amount;
-            UpdateScoreUI(playerID);
-        }
-    }
+//        UpdateAllScoreUI();
+//    }
 
-    public void DecreaseScore(int playerID, int amount)
-    {
-        if (playerID >= 0 && playerID < PlayerScore.Count)
-        {
-            PlayerScore[playerID] -= amount;
-            UpdateScoreUI(playerID);
-        }
-    }
 
-    private void UpdateScoreUI(int playerID)
-    {
-        _playerScoreTexts[playerID].text = $"Player {playerID + 1}: {PlayerScore[playerID]}";
-    }
+//    private void Update()
+//    {
+//        for (int i = 0; i < PlayerScore.Count; i++)
+//        {
+//            if (Input.GetKeyDown(KeyCode.Alpha1 + i))
+//            {
+//                IncreaseScore(i, 10);
+//            }
+//        }
+//    }
 
-    private void UpdateAllScoreUI()
-    {
-        for (int i = 0; i < PlayerScore.Count; i++)
-        {
-            UpdateScoreUI(i);
-        }
-    }
-}
+//    public void IncreaseScore(int playerID, int amount)
+//    {
+//        if (playerID >= 0 && playerID < PlayerScore.Count)
+//        {
+//            PlayerScore[playerID] += amount;
+//            UpdateScoreUI(playerID);
+//        }
+//    }
+
+//    public void DecreaseScore(int playerID, int amount)
+//    {
+//        if (playerID >= 0 && playerID < PlayerScore.Count)
+//        {
+//            PlayerScore[playerID] -= amount;
+//            UpdateScoreUI(playerID);
+//        }
+//    }
+
+//    private void UpdateScoreUI(int playerID)
+//    {
+//        _playerScoreTexts[playerID].text = $"Player {playerID + 1}: {PlayerScore[playerID]}";
+//    }
+
+//    private void UpdateAllScoreUI()
+//    {
+//        for (int i = 0; i < PlayerScore.Count; i++)
+//        {
+//            UpdateScoreUI(i);
+//        }
+//    }
+//}
