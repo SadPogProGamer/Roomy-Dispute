b5f3af08e68e0fd932c9607ec8b5e3c99749dfa6
FindMoreLuck <liam.carlier15@gmail.com>
2025-05-22 15:59:23 +0200

    fix points

--- Assets/Scripts/WIP[Liam]/Room/ItemStats.cs
+++ Assets/Scripts/WIP[Liam]/Room/ItemStats.cs
@@ -1,103 +1,3 @@
-////using UnityEngine;
-////using UnityEngine.UI;
-
-////public class ItemStats : MonoBehaviour
-////{
-////    public int HP, Cost, Points;
-////    public bool IsGlass, IsWood, IsPlaced;
-////    public float PlacementTime, Timer;
-////    public GameObject PlayerPhone;
-////    private bool phoneHasNotBeenActivated = true;
-
-
-////    // Update is called once per frame
-////    void Update()
-////    {
-////        if (HP <= 0) Destroy(gameObject);
-
-////        if (IsPlaced && Timer < PlacementTime && phoneHasNotBeenActivated)
-////        {
-////            Timer += Time.deltaTime;
-////            PlayerPhone.SetActive(true);
-
-////            for (int childIndex = 0; childIndex< PlayerPhone.transform.childCount - 1; childIndex++)
-////            {
-////                if(PlayerPhone.transform.GetChild(childIndex).gameObject.activeSelf)
-////                    PlayerPhone.transform.GetChild(childIndex).gameObject.SetActive(false);
-////            }
-
-////            PlayerPhone.transform.GetChild(PlayerPhone.transform.childCount - 1).gameObject.SetActive(true);
-////            PlayerPhone.transform.GetChild(PlayerPhone.transform.childCount - 1).GetComponent<Slider>().value = PlacementTime-Timer;
-////            PlayerPhone.transform.GetChild(PlayerPhone.transform.childCount - 1).GetComponent<Slider>().maxValue = PlacementTime;
-////        }
-
-////        if (Timer >= PlacementTime && phoneHasNotBeenActivated)
-////        {
-////            for (int childIndex = 0; childIndex < PlayerPhone.transform.childCount - 1; childIndex++)
-////            {
-////                if (!PlayerPhone.transform.GetChild(childIndex).gameObject.activeSelf)
-////                    PlayerPhone.transform.GetChild(childIndex).gameObject.SetActive(true);
-////            }
-
-////            PlayerPhone.transform.GetChild(PlayerPhone.transform.childCount - 1).gameObject.SetActive(false);
-////            PlayerPhone.transform.parent.GetComponent<ButtonSelect>().CancelAction();
-////            phoneHasNotBeenActivated = false;
-////        }
-////    }
-////}
-//using UnityEngine;
-//using UnityEngine.UI;
-
-//public class ItemStats : MonoBehaviour
-//{
-//    public int HP, Cost, Points;
-//    public bool IsGlass, IsWood, IsPlaced;
-//    public float PlacementTime, Timer;
-//    public GameObject PlayerPhone;
-//    private bool phoneHasNotBeenActivated = true;
-
-//    private bool pointsAdded = false;  // To prevent adding points multiple times
-
-//    void Update()
-//    {
-//        if (HP <= 0) Destroy(gameObject);
-
-//        if (IsPlaced && !pointsAdded)
-//        {
-//            ScoreManager.Instance.AddPoints(Points);
-//            pointsAdded = true;
-//        }
-
-//        if (IsPlaced && Timer < PlacementTime && phoneHasNotBeenActivated)
-//        {
-//            Timer += Time.deltaTime;
-//            PlayerPhone.SetActive(true);
-
-//            for (int childIndex = 0; childIndex < PlayerPhone.transform.childCount - 1; childIndex++)
-//            {
-//                if (PlayerPhone.transform.GetChild(childIndex).gameObject.activeSelf)
-//                    PlayerPhone.transform.GetChild(childIndex).gameObject.SetActive(false);
-//            }
-
-//            PlayerPhone.transform.GetChild(PlayerPhone.transform.childCount - 1).gameObject.SetActive(true);
-//            PlayerPhone.transform.GetChild(PlayerPhone.transform.childCount - 1).GetComponent<Slider>().value = Timer;
-//            PlayerPhone.transform.GetChild(PlayerPhone.transform.childCount - 1).GetComponent<Slider>().maxValue = PlacementTime;
-//        }
-
-//        if (Timer >= PlacementTime && phoneHasNotBeenActivated)
-//        {
-//            for (int childIndex = 0; childIndex < PlayerPhone.transform.childCount - 1; childIndex++)
-//            {
-//                if (!PlayerPhone.transform.GetChild(childIndex).gameObject.activeSelf)
-//                    PlayerPhone.transform.GetChild(childIndex).gameObject.SetActive(true);
-//            }
-
-//            PlayerPhone.transform.GetChild(PlayerPhone.transform.childCount - 1).gameObject.SetActive(false);
-//            PlayerPhone.transform.parent.GetComponent<ButtonSelect>().CancelAction();
-//            phoneHasNotBeenActivated = false;
-//        }
-//    }
-//}
 using UnityEngine;
 using UnityEngine.UI;
 
@@ -108,21 +8,49 @@ public class ItemStats : MonoBehaviour
     public float PlacementTime, Timer;
     public GameObject PlayerPhone;
     private bool phoneHasNotBeenActivated = true;
+    public int PlayerIndex;
+    private bool pointsAdded = false;  // To prevent adding points multiple times
 
-    private bool pointsAdded = false;
 
-    public int playerIndex;  // NEW: Which player owns this item (0-3)
-
+    // Update is called once per frame
     void Update()
     {
         if (HP <= 0) Destroy(gameObject);
 
         if (IsPlaced && !pointsAdded)
         {
-            ScoreManager.Instance.AddPoints(playerIndex, Points);
+            ScoreManager.Instance.AddPoints(PlayerIndex, Points);
             pointsAdded = true;
         }
 
-        // Your existing Update code for timer and phone UI here ...
+        if (IsPlaced && Timer < PlacementTime && phoneHasNotBeenActivated)
+        {
+            Timer += Time.deltaTime;
+            PlayerPhone.SetActive(true);
+
+            for (int childIndex = 0; childIndex < PlayerPhone.transform.childCount - 1; childIndex++)
+            {
+                if (PlayerPhone.transform.GetChild(childIndex).gameObject.activeSelf)
+                    PlayerPhone.transform.GetChild(childIndex).gameObject.SetActive(false);
+            }
+
+            PlayerPhone.transform.GetChild(PlayerPhone.transform.childCount - 1).gameObject.SetActive(true);
+            PlayerPhone.transform.GetChild(PlayerPhone.transform.childCount - 1).GetComponent<Slider>().value = PlacementTime - Timer;
+            PlayerPhone.transform.GetChild(PlayerPhone.transform.childCount - 1).GetComponent<Slider>().maxValue = PlacementTime;
+        }
+
+        if (Timer >= PlacementTime && phoneHasNotBeenActivated)
+        {
+            for (int childIndex = 0; childIndex < PlayerPhone.transform.childCount - 1; childIndex++)
+            {
+                if (!PlayerPhone.transform.GetChild(childIndex).gameObject.activeSelf)
+                    PlayerPhone.transform.GetChild(childIndex).gameObject.SetActive(true);
+            }
+
+            PlayerPhone.transform.GetChild(PlayerPhone.transform.childCount - 1).gameObject.SetActive(false);
+            PlayerPhone.transform.parent.GetComponent<ButtonSelect>().CancelAction();
+            phoneHasNotBeenActivated = false;
+        }
     }
 }
+
