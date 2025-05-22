7eeb8390155c98edf1bf5d86b9e320698b44d00c
lg6200 <gouwyloic@gmail.com>
2025-05-22 15:44:50 +0200

    scoreplswork

--- Assets/Scripts/WIP[Loic]/ScoreManager.cs
+++ Assets/Scripts/WIP[Loic]/ScoreManager.cs
@@ -1,64 +1,140 @@
+////using UnityEngine;
+////using TMPro;
+////using System.Collections.Generic;
+
+////public class ScoreManager : MonoBehaviour
+////{
+////    [SerializeField] private TextMeshProUGUI[] _playerScoreTexts;
+
+////    public List<int> PlayerScore = new List<int>();
+
+
+////    private void Start()
+////    {
+////        for (int i = 0; i < _playerScoreTexts.Length; i++)
+////        {
+////            PlayerScore.Add(0);
+////        }
+
+////        UpdateAllScoreUI();
+////    }
+
+
+////    private void Update()
+////    {
+////        for (int i = 0; i < PlayerScore.Count; i++)
+////        {
+////            if (Input.GetKeyDown(KeyCode.Alpha1 + i))
+////            {
+////                IncreaseScore(i, 10);
+////            }
+////        }
+////    }
+
+////    public void IncreaseScore(int playerID, int amount)
+////    {
+////        if (playerID >= 0 && playerID < PlayerScore.Count)
+////        {
+////            PlayerScore[playerID] += amount;
+////            UpdateScoreUI(playerID);
+////        }
+////    }
+
+////    public void DecreaseScore(int playerID, int amount)
+////    {
+////        if (playerID >= 0 && playerID < PlayerScore.Count)
+////        {
+////            PlayerScore[playerID] -= amount;
+////            UpdateScoreUI(playerID);
+////        }
+////    }
+
+////    private void UpdateScoreUI(int playerID)
+////    {
+////        _playerScoreTexts[playerID].text = $"Player {playerID + 1}: {PlayerScore[playerID]}";
+////    }
+
+////    private void UpdateAllScoreUI()
+////    {
+////        for (int i = 0; i < PlayerScore.Count; i++)
+////        {
+////            UpdateScoreUI(i);
+////        }
+////    }
+////}
 //using UnityEngine;
 //using TMPro;
-//using System.Collections.Generic;
 
 //public class ScoreManager : MonoBehaviour
 //{
-//    [SerializeField] private TextMeshProUGUI[] _playerScoreTexts;
+//    public static ScoreManager Instance;  // Singleton for easy access
 
-//    public List<int> PlayerScore = new List<int>();
+//    public int playerScore = 0;
+//    public TextMeshProUGUI scoreText;
 
+//    private void Awake()
+//    {
+//        // Simple singleton pattern
+//        if (Instance == null)
+//            Instance = this;
+//        else
+//            Destroy(gameObject);
+//    }
 
 //    private void Start()
 //    {
-//        for (int i = 0; i < _playerScoreTexts.Length; i++)
-//        {
-//            PlayerScore.Add(0);
-//        }
-
-//        UpdateAllScoreUI();
+//        UpdateScoreText();
 //    }
 
-
-//    private void Update()
+//    public void AddPoints(int points)
 //    {
-//        for (int i = 0; i < PlayerScore.Count; i++)
-//        {
-//            if (Input.GetKeyDown(KeyCode.Alpha1 + i))
-//            {
-//                IncreaseScore(i, 10);
-//            }
-//        }
+//        playerScore += points;
+//        UpdateScoreText();
 //    }
 
-//    public void IncreaseScore(int playerID, int amount)
+//    private void UpdateScoreText()
 //    {
-//        if (playerID >= 0 && playerID < PlayerScore.Count)
-//        {
-//            PlayerScore[playerID] += amount;
-//            UpdateScoreUI(playerID);
-//        }
-//    }
-
-//    public void DecreaseScore(int playerID, int amount)
-//    {
-//        if (playerID >= 0 && playerID < PlayerScore.Count)
-//        {
-//            PlayerScore[playerID] -= amount;
-//            UpdateScoreUI(playerID);
-//        }
-//    }
-
-//    private void UpdateScoreUI(int playerID)
-//    {
-//        _playerScoreTexts[playerID].text = $"Player {playerID + 1}: {PlayerScore[playerID]}";
-//    }
-
-//    private void UpdateAllScoreUI()
-//    {
-//        for (int i = 0; i < PlayerScore.Count; i++)
-//        {
-//            UpdateScoreUI(i);
-//        }
+//        if (scoreText != null)
+//            scoreText.text = "Score: " + playerScore;
 //    }
 //}
+using UnityEngine;
+using TMPro;
+
+public class ScoreManager : MonoBehaviour
+{
+    public static ScoreManager Instance;
+
+    public int[] playerScores = new int[4];  // Array for 4 players
+    public TextMeshProUGUI[] scoreTexts;     // Assign 4 TextMeshProUGUI elements, one for each player
+
+    private void Awake()
+    {
+        if (Instance == null)
+            Instance = this;
+        else
+            Destroy(gameObject);
+    }
+
+    private void Start()
+    {
+        for (int i = 0; i < scoreTexts.Length; i++)
+        {
+            UpdateScoreText(i);
+        }
+    }
+
+    public void AddPoints(int playerIndex, int points)
+    {
+        if (playerIndex < 0 || playerIndex >= playerScores.Length) return;
+
+        playerScores[playerIndex] += points;
+        UpdateScoreText(playerIndex);
+    }
+
+    private void UpdateScoreText(int playerIndex)
+    {
+        if (scoreTexts != null && scoreTexts.Length > playerIndex && scoreTexts[playerIndex] != null)
+            scoreTexts[playerIndex].text = $"Player {playerIndex + 1} Score: {playerScores[playerIndex]}";
+    }
+}
