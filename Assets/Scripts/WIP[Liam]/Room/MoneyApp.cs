309130c51b79ab27d29c7d39431df1ff0a862482
FindMoreLuck <liam.carlier15@gmail.com>
2025-05-25 14:03:51 +0200

    made some changes to make things better

--- Assets/Scripts/WIP[Liam]/Room/MoneyApp.cs
+++ Assets/Scripts/WIP[Liam]/Room/MoneyApp.cs
@@ -3,23 +3,31 @@ using UnityEngine;
 public class MoneyApp : MonoBehaviour
 {
     private int _buttonIndex, _previousButtonIndex;
+    [SerializeField]
+    private GameObject _buttonsParent;
     // Update is called once per frame
 
     private void Start()
     {
-        _buttonIndex = Random.Range(0, 8);
+        _buttonIndex = Random.Range(0, 3);
+        _previousButtonIndex = -1;//-1 so that it doesnt do the while loop at the start
     }
     void Update()
     {
+        while (_buttonIndex == _previousButtonIndex)
+        {
+
+        }
+
         if (_buttonIndex != _previousButtonIndex)
         {
-            for (int childIndex = 0; childIndex < transform.childCount; childIndex++)
+            for (int childIndex = 0; childIndex < _buttonsParent.transform.childCount; childIndex++)
             {
                 if(childIndex == _buttonIndex)
                 {
-                    transform.GetChild(childIndex).gameObject.SetActive(true);
+                    _buttonsParent.transform.GetChild(childIndex).gameObject.SetActive(true);
                 }
-                else { transform.GetChild(childIndex).gameObject.SetActive(false); }
+                else { _buttonsParent.transform.GetChild(childIndex).gameObject.SetActive(false); }
             }
         }
     }
@@ -31,34 +39,43 @@ public class MoneyApp : MonoBehaviour
         {
 
         }
-        else;
     }
     public void OnDown()
     {
+        if (_buttonIndex == 1)
+        {
 
+        }
     }
     public void OnLeft()
     {
+        if (_buttonIndex == 2)
+        {
 
+        }
     }
     public void OnRight()
     {
+        if (_buttonIndex == 3)
+        {
 
+        }
     }
-    public void OnA()
-    {
+    //public void OnA()
+    //{
 
-    }
-    public void OnX()
-    {
+    //}
+    //public void OnX()
+    //{
 
-    }
-    public void OnY()
-    {
+    //}
+    //public void OnY()
+    //{
 
-    }
-    public void OnB()
-    {
+    //}
+    //public void OnB()
+    //{
 
-    }
+    //}
+
 }
