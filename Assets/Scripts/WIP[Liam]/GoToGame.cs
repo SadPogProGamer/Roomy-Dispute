5e60bf3a614de79e829b1390cf9afc4a7d49399a
FindMoreLuck <liam.carlier15@gmail.com>
2025-05-25 21:26:58 +0200

    bunch of changes

---
+++ Assets/Scripts/WIP[Liam]/GoToGame.cs
@@ -0,0 +1,18 @@
+using UnityEngine;
+using UnityEngine.InputSystem;
+using UnityEngine.SceneManagement;
+
+public class GoToGame : MonoBehaviour
+{
+
+
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Gamepad.current.buttonSouth.wasPressedThisFrame)
+        {
+            SceneManager.LoadScene(1);
+        }
+    }
+}
--- Assets/Scripts/WIP[Liam]/Room/CanvasRightPosition.cs
+++ Assets/Scripts/WIP[Liam]/Room/CanvasRightPosition.cs
@@ -5,6 +5,6 @@ public class CanvasRightposition : MonoBehaviour
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Update()
     {
-        transform.rotation =Quaternion.identity;
+        transform.rotation = Quaternion.Euler(0,transform.parent.parent.localRotation.y,0);
     }
 }
