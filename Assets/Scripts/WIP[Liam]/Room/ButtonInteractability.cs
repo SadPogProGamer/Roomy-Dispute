c1968dc9dc40310d236743b53a1a453dd5e672cc
FindMoreLuck <liam.carlier15@gmail.com>
2025-05-27 18:24:07 +0200

    Update ButtonInteractability.cs

--- Assets/Scripts/WIP[Liam]/Room/ButtonInteractability.cs
+++ Assets/Scripts/WIP[Liam]/Room/ButtonInteractability.cs
@@ -8,14 +8,19 @@ public class ButtonInteractability : MonoBehaviour
 {
     [SerializeField]
     private GameObject _prefab, _moneyManager, _playerIndexStartingFrom0;
+    private bool _isPlacable, _placableIsInteractable;
 
     // Update is called once per frame
     void Update()
     {
-        if (_moneyManager.GetComponent<ButtonSelect>().MoneyManager.GetComponent<MoneyManager>().PlayerMoney[_playerIndexStartingFrom0.GetComponent<PlayerPointer>().PlayerIndex]>= _prefab.GetComponent<ItemStats>().Cost)
-            GetComponent<Button>().interactable = true;
-        else 
-            GetComponent<Button>().interactable = false;
+        if(!_isPlacable|| (_isPlacable && _placableIsInteractable))
+        {
+            if (_moneyManager.GetComponent<ButtonSelect>().MoneyManager.GetComponent<MoneyManager>().PlayerMoney[_playerIndexStartingFrom0.GetComponent<PlayerPointer>().PlayerIndex] >= _prefab.GetComponent<ItemStats>().Cost)
+                GetComponent<Button>().interactable = true;
+            else
+                GetComponent<Button>().interactable = false;
+        }
+
 
         if (_prefab.tag.Contains("Placable"))
         {
@@ -23,20 +28,21 @@ public class ButtonInteractability : MonoBehaviour
             List<GameObject> listOfTvTables = GameObject.FindGameObjectsWithTag("Item/Long").Where(table => table.transform.childCount > 0).ToList();
             List<GameObject> listOfDrawers = GameObject.FindGameObjectsWithTag("Item/Small").Where(table => table.transform.childCount > 0).ToList();
 
-            if (_prefab.tag.Contains("Big"))
-            {
-                CheckIfPlacableOnHasEnoughSlots(3, listOfTables, listOfTvTables, listOfDrawers);
-            }
-            if (_prefab.tag.Contains("Medium"))
-            {
-                CheckIfPlacableOnHasEnoughSlots(2, listOfTables, listOfTvTables, listOfDrawers);
-            }
-            if (_prefab.tag.Contains("Small"))
-            {
-                CheckIfPlacableOnHasEnoughSlots(1, listOfTables, listOfTvTables, listOfDrawers);
-            }
+            _isPlacable = true;
 
-        }
+                if (_prefab.tag.Contains("Big"))
+                {
+                    CheckIfPlacableOnHasEnoughSlots(3, listOfTables, listOfTvTables, listOfDrawers);
+                }
+                if (_prefab.tag.Contains("Medium"))
+                {
+                    CheckIfPlacableOnHasEnoughSlots(2, listOfTables, listOfTvTables, listOfDrawers);
+                }
+                if (_prefab.tag.Contains("Small"))
+                {
+                    CheckIfPlacableOnHasEnoughSlots(1, listOfTables, listOfTvTables, listOfDrawers);
+                }
+        } 
     }
 
     private void CheckIfPlacableOnHasEnoughSlots(int slotCount, List<GameObject> tables, List<GameObject> tvTables, List<GameObject> drawers)
@@ -45,22 +51,13 @@ public class ButtonInteractability : MonoBehaviour
         List<GameObject> tvTablesAvailable = tvTables.Where(tvTable => tvTable.transform.childCount <= 6 - slotCount).ToList();
         List<GameObject> drawersAvailable = drawers.Where(drawer => drawer.transform.childCount <= 4 - slotCount).ToList();
 
-        if (drawersAvailable.Count != 0)
-        {
-            GetComponent<Button>().interactable = true;
-        }
-        else 
-        if(tvTablesAvailable.Count != 0)
+        if (drawersAvailable.Count != 0 || tvTablesAvailable.Count != 0 || tablesAvailable.Count != 0)
         {
             GetComponent<Button>().interactable = true;
         }
         else
-        if(tablesAvailable.Count != 0)
         {
-            GetComponent<Button>().interactable = true;
-        }
-        else
             GetComponent<Button>().interactable = false;
-
+        }
     }
 }
