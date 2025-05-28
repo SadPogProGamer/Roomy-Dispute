a459972c147c030b5a813dfe89362c914043b17a
FindMoreLuck <liam.carlier15@gmail.com>
2025-05-28 16:18:18 +0200

    Update CubeIsTriggerable.cs

--- Assets/Scripts/WIP[Liam]/Room/CubeIsTriggerable.cs
+++ Assets/Scripts/WIP[Liam]/Room/CubeIsTriggerable.cs
@@ -6,12 +6,6 @@ public class CubeIsTriggerable : MonoBehaviour
     public bool IsTriggerable;
     private GameObject _currentCollider;
 
-    //private void OnTriggerExit(Collider other)
-    //{
-    //    if (other.tag.Contains("Item"))
-    //        IsTriggerable = true;
-    //}
-
     private void Update()
     {
         if (_currentCollider.IsDestroyed())
