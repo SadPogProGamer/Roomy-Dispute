e248dbafebf5969a2b9bbf069953a3f11be75b7e
FindMoreLuck <liam.carlier15@gmail.com>
2025-05-29 00:07:00 +0200

    changes on winscreen

--- Assets/Scripts/WIP[Liam]/PlayerSpawn.cs
+++ Assets/Scripts/WIP[Liam]/PlayerSpawn.cs
@@ -1,5 +1,6 @@
 using NUnit.Framework;
 using UnityEngine;
+using UnityEngine.InputSystem;
 
 public class PlayerSpawn : MonoBehaviour
 {
@@ -8,11 +9,14 @@ public class PlayerSpawn : MonoBehaviour
     [SerializeField] 
     private Transform[] _spawnPoints;
     [SerializeField]
+    private Material[] _materials;
+    [SerializeField]
     private int _playerCount;
 
     void Awake()
     {
+        _playerCount = Gamepad.all.Count;
         for (int playerIndex = 0; playerIndex < _playerCount; playerIndex++)
-            Instantiate(_playerPrefab, new Vector3(_spawnPoints[playerIndex].position.x, _playerPrefab.transform.position.y, _spawnPoints[playerIndex].position.z), _spawnPoints[playerIndex].rotation); 
+            Instantiate(_playerPrefab, new Vector3(_spawnPoints[playerIndex].position.x, _playerPrefab.transform.position.y, _spawnPoints[playerIndex].position.z), _spawnPoints[playerIndex].rotation).GetComponent<MeshRenderer>().material = _materials[playerIndex]; 
     }
 }
