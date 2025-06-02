16cd4101ecd7698f9b0b12762357d1040e4e9b4a
FindMoreLuck <liam.carlier15@gmail.com>
2025-06-02 20:23:46 +0200

    Update ItemPlacement.cs

--- Assets/Scripts/WIP[Liam]/Room/ItemPlacement.cs
+++ Assets/Scripts/WIP[Liam]/Room/ItemPlacement.cs
@@ -184,6 +184,7 @@ public class ItemPlacement : MonoBehaviour
             Item.GetComponent<ItemStats>().PlayerPhone.transform.parent.GetComponent<ButtonSelect>().CancelAction();
             Item.GetComponent<ItemStats>().PlayerPhone.transform.parent.GetComponent<ButtonSelect>().MoneyManager.GetComponent<MoneyManager>().IncreaseMoney(GetComponent<PlayerPointer>().PlayerIndex, Item.GetComponent<ItemStats>().Cost);
             Item.GetComponent<ItemStats>().PlayerPhone.transform.parent.GetComponent<ButtonSelect>().SetFurnitureBack();
+            Item.GetComponent<ItemStats>().PlayerPhone.transform.parent.GetComponent<ButtonSelect>().Boarders.SetActive(true);
 
             Destroy(Item);
             SetPointerBackToOrigin();
