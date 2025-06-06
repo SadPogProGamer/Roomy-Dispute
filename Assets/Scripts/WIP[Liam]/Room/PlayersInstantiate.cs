8bc74a435173957126e2ee490ec1648ff3eea62d
SadPogProGamer <cloetsenna@gmail.com>
2025-06-06 15:28:59 +0200

    uwu

--- Assets/Scripts/WIP[Liam]/Room/PlayersInstantiate.cs
+++ Assets/Scripts/WIP[Liam]/Room/PlayersInstantiate.cs
@@ -9,50 +9,54 @@ public class PlayersInstantiate : MonoBehaviour
     private GameObject _prefab, _moneyManager;
     [SerializeField]
     private GameObject[] _room;
-    [HideInInspector] public int _playerCount;
     [SerializeField]
     private ReadyManager _readyManager;
 
+    private int _playerLimit = 4; // You can set this in the Inspector
+
     public Material[] _playerMaterials;
-
     public List<GameObject> SpawnedPlayers { get; private set; } = new List<GameObject>();
+    [HideInInspector] public int _playerCount;
 
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Awake()
     {
-        _playerCount = Gamepad.all.Count;
+        _playerCount = Mathf.Min(Gamepad.all.Count, _playerLimit);
 
         for (int plyrcnt = 0; plyrcnt < _playerCount; plyrcnt++)
         {
             GameObject player = Instantiate(_prefab);
             player.transform.GetChild(0).GetComponent<ButtonSelect>().MoneyManager = _moneyManager;
+
             if (plyrcnt % 2 != 0)
             {
-                player.transform.GetChild(0).GetChild(0).GetComponent<RectTransform>().anchoredPosition = new Vector2(-player.transform.GetChild(0).GetChild(0).GetComponent<RectTransform>().anchoredPosition.x, player.transform.GetChild(0).GetChild(0).GetComponent<RectTransform>().anchoredPosition.y);
-                player.transform.GetChild(0).GetChild(1).GetComponent<RectTransform>().anchoredPosition = new Vector2(-player.transform.GetChild(0).GetChild(1).GetComponent<RectTransform>().anchoredPosition.x, player.transform.GetChild(0).GetChild(1).GetComponent<RectTransform>().anchoredPosition.y);
+                var rt0 = player.transform.GetChild(0).GetChild(0).GetComponent<RectTransform>();
+                var rt1 = player.transform.GetChild(0).GetChild(1).GetComponent<RectTransform>();
+                rt0.anchoredPosition = new Vector2(-rt0.anchoredPosition.x, rt0.anchoredPosition.y);
+                rt1.anchoredPosition = new Vector2(-rt1.anchoredPosition.x, rt1.anchoredPosition.y);
             }
             if (plyrcnt > 1)
             {
-                player.transform.GetChild(0).GetChild(0).GetComponent<RectTransform>().anchoredPosition = new Vector2(player.transform.GetChild(0).GetChild(0).GetComponent<RectTransform>().anchoredPosition.x, -player.transform.GetChild(0).GetChild(0).GetComponent<RectTransform>().anchoredPosition.y);
-                player.transform.GetChild(0).GetChild(1).GetComponent<RectTransform>().anchoredPosition = new Vector2(player.transform.GetChild(0).GetChild(1).GetComponent<RectTransform>().anchoredPosition.x, -player.transform.GetChild(0).GetChild(1).GetComponent<RectTransform>().anchoredPosition.y);
+                var rt0 = player.transform.GetChild(0).GetChild(0).GetComponent<RectTransform>();
+                var rt1 = player.transform.GetChild(0).GetChild(1).GetComponent<RectTransform>();
+                rt0.anchoredPosition = new Vector2(rt0.anchoredPosition.x, -rt0.anchoredPosition.y);
+                rt1.anchoredPosition = new Vector2(rt1.anchoredPosition.x, -rt1.anchoredPosition.y);
             }
 
             for (int border = 0; border < player.transform.GetChild(0).GetChild(1).childCount; border++)
                 player.transform.GetChild(0).GetChild(1).GetChild(border).GetComponent<Image>().color = _playerMaterials[plyrcnt].color * Color.gray;
+
             player.transform.GetChild(1).GetComponent<PlayerPointer>().PlayerIndex = plyrcnt;
             player.transform.GetChild(1).GetComponent<PlayerPointer>().Room = _room;
 
-
             Renderer rend = player.GetComponentInChildren<Renderer>();
             if (rend != null && plyrcnt < _playerMaterials.Length)
-            {
                 rend.material = _playerMaterials[plyrcnt];
-            }
 
             SpawnedPlayers.Add(player);
             _readyManager.Players.Add(player);
         }
     }
+
     public GameObject GetPlayer(int index)
     {
         if (index >= 0 && index < SpawnedPlayers.Count)
